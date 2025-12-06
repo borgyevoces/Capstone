14bda6297a85bca7c8c4b627b0fdd430fdc853af
robbyrossmajaba16-cloud <robbyrossmajaba16@gmail.com>
2025-12-06 17:11:54 +0800

    Update settings.py

--- Capstone/settings.py
+++ Capstone/settings.py
@@ -184,6 +184,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # or use a wildcard 'https://*.ngrok-free.app' if it changes.
 CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:8000',
+    'https://kabsueats.onrender.com',
     # If you run the dev server on a different port or host, update SITE_URL above
 ]
 # ===================================================================================
