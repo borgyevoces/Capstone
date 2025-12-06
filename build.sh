eefc12fc30730ad1a1ec6fdfec2769611b281892
robbyrossmajaba16-cloud <robbyrossmajaba16@gmail.com>
2025-12-06 15:38:10 +0800

    Fix datetime migration issue

--- build.sh
+++ build.sh
@@ -5,5 +5,11 @@ pip install --upgrade pip
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
+
+# Fake the problematic migration
+python manage.py migrate webapp 0041 --fake
+
+# Run all other migrations
 python manage.py migrate
+
 python manage.py create_default_superuser
